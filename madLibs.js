19c959321577e0749166cfec8e3059b1b21fec5b
edwinaguila <edwinaguila00@gmail.com>
2025-03-28 16:05:45 +0000

    Added a boolean?

--- madLibs.js
+++ madLibs.js
@@ -9,11 +9,15 @@ const object = {
     age: 42,
 }
 
+let terrain1 = '';
+let terrain2 = terrain1 || "sea" 
+
+
 let chest = "Keyblade";
 let treasure = "Kingdom Hearts";
 
 
-const story = `On a ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. The endless sea stretched endlessly before us. Just then, we spotted a mysterious ${chest} floating in the water. 'Could it be ${treasure}?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age}, ordered us to grab it, so ${theCrew} hauled it aboard and opened it. Did you know 5 × 5 equals ${mathTreasure}? Incredible, right?`;
+const story = `On a ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. The endless ${terrain2} stretched endlessly before us. Just then, we spotted a mysterious ${chest} floating in the water. 'Could it be ${treasure}?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age}, ordered us to grab it, so ${theCrew} hauled it aboard and opened it. Did you know 5 × 5 equals ${mathTreasure}? Incredible, right?`;
 
 document.getElementById("madlibs-output").textContent = story;
 console.log(story);
