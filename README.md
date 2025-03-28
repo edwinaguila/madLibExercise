e54ccf4b57096e4ecd0b4e9e48ef808c4ebaceef
edwinaguila <edwinaguila00@gmail.com>
2025-03-28 13:45:31 +0000

    Added number data type and updated markdown

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # madLibExercise
+
+## Objective
+* Learning template literals.
+* Practice variable and declaration assignment.
+* Understand different data types.
+* Defining objects with properties and accesing them with dot notation.
+* Understand how to write and evaulate boolean expressions.
+
+## Outcome
+A heading with `Mad Libs Exercise` with our own MadLibs story beneath.
--- madLibs.js
+++ madLibs.js
@@ -1,22 +1,19 @@
 let theCrew = 'we';
 const theBlackPearl = 'the Highwind';
 let timeOfDay = 'stormy afternoon';
-
 let pirateShip = "rubber duck";
+const mathTreasure = 5 * 5;
 
 const object = {
     name: "Captain Hook",
     age: 42,
 }
 
-if (object.name + object.age) {
-    console.log("Captain Hook");
-} else {
-    console.log('Alligator')
-}
-
 let chest = "Keyblade";
 let treasure = "Kingdom Hearts";
 
 
-const story = `On a ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. The endless sea stretched endlessly before us. Just then, we spotted a mysterious ${chest} floating in the water. 'Could it be ${treasure}?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age}, ordered us to grab it, so ${theCrew} hauled it aboard and opened it. Did you know 5 × 5 equals 25? Incredible, right?`
+const story = `On a ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. The endless sea stretched endlessly before us. Just then, we spotted a mysterious ${chest} floating in the water. 'Could it be ${treasure}?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age}, ordered us to grab it, so ${theCrew} hauled it aboard and opened it. Did you know 5 × 5 equals ${mathTreasure}? Incredible, right?`;
+
+document.getElementById("madlibs-output").textContent = story;
+console.log(story);
