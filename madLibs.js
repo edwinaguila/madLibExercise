3354e84caceac3c61d820c02249d8323f331dea8
edwinaguila <edwinaguila00@gmail.com>
2025-03-28 01:54:48 +0000

    Fixed code to have const story and figuring out boolean

--- madLibs.js
+++ madLibs.js
@@ -1,14 +1,22 @@
 let theCrew = 'we';
-const theBlackPearl = 'Highwind';
-let timeOfDay = "stormy afternoon";
+const theBlackPearl = 'the Highwind';
+let timeOfDay = 'stormy afternoon';
+
 let pirateShip = "rubber duck";
+
 const object = {
     name: "Captain Hook",
     age: 42,
 }
 
-console.log(`One ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. 
-    The endless sea stretched endlessly before us. Just then, we spotted a mysterious chest floating in the water. 
-    'Could it be treasure?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age},
-    ordered us to grab it, so ${theCrew} hauled it aboard and opened it. 
-    Did you know 5 × 5 equals 25? Incredible, right?`)
+if (object.name + object.age) {
+    console.log("Captain Hook");
+} else {
+    console.log('Alligator')
+}
+
+let chest = "Keyblade";
+let treasure = "Kingdom Hearts";
+
+
+const story = `On a ${timeOfDay}, ${theCrew} set sail on a ${pirateShip} named ${theBlackPearl}. The endless sea stretched endlessly before us. Just then, we spotted a mysterious ${chest} floating in the water. 'Could it be ${treasure}?' ${object.name} exclaimed. ${object.name}, knowing he was ${object.age}, ordered us to grab it, so ${theCrew} hauled it aboard and opened it. Did you know 5 × 5 equals 25? Incredible, right?`
